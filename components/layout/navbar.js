967ef12ff970119697c45e3593485fc5feee418c
calebbabin <object.caleb@gmail.com>
2022-06-07 11:44:33 -0500

    link navbar logo to home

--- components/layout/navbar.js
+++ components/layout/navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
 
 	return (<div className='bg-base text-content theme-light'>
 		<nav className="max-w-7xl mx-auto w-11/12 px-2 py-4 box-border flex justify-between">
-			<h2 className="my-0 font-semibold"><SmartLink href="https://www.holaplex.com/">👋 Holaplex</SmartLink><span className="text-gray-600"> - <SmartLink href="/">careers</SmartLink></span></h2>
+			<h2 className="my-0 font-semibold"><SmartLink href="/">👋 Holaplex</SmartLink><span className="text-gray-400"> - <SmartLink href="/">careers</SmartLink></span></h2>
 			<ul id="navItems" className="hidden md:block">
 				<NavItem href="https://www.holaplex.com/">Learn More</NavItem>
 			</ul>
