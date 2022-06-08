c9fedd92d840e3691a48fff4f57381f4e80371bd
calebbabin <object.caleb@gmail.com>
2022-06-08 10:32:09 -0500

    minor updates

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
 					<script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
 
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
-					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
 					<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap" rel="stylesheet" />
 				</Head>
 				<body className='bg-black'>
